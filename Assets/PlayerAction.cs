9411be3f4a4960cff8af722ed1d1de5770ab7263
HaeSeungJee <wlgotmd123@naver.com>
2023-02-09 03:03:46 +0900

    B21
    조사 액션 추가

--- Assets/PlayerAction.cs
+++ Assets/PlayerAction.cs
@@ -8,6 +8,8 @@ public class PlayerAction : MonoBehaviour
     float h;
     float v;
     bool isHorizonMove;
+    Vector3 dirVec;
+    GameObject scanObject;
 
     Rigidbody2D rb;
     Animator anim;
@@ -49,6 +51,19 @@ public class PlayerAction : MonoBehaviour
         else
             anim.SetBool("isChange", false);
         
+        // Direction
+        if (vDown && v == 1)
+            dirVec = Vector3.up;
+        else if (vDown && v == -1)
+            dirVec = Vector3.down;
+        else if (hDown && h == -1)
+            dirVec = Vector3.left;
+        else if (hDown && h == 1)
+            dirVec = Vector3.right;
+
+        // Scan Object
+        if (Input.GetButtonDown("Jump") && scanObject != null)
+            Debug.Log(" [" + scanObject.name + "] 입니다.");
     }
 
     private void FixedUpdate()
@@ -56,5 +71,14 @@ public class PlayerAction : MonoBehaviour
         // Move
         Vector2 moveVec = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v);
         rb.velocity = moveVec * speed;
+
+        // Ray
+        Debug.DrawRay(rb.position, dirVec * 0.7f, new Color(0,1,0));
+        RaycastHit2D rayHit = Physics2D.Raycast(rb.position, dirVec, 0.7f,  LayerMask.GetMask("Object"));
+
+        if (rayHit.collider != null) {
+            scanObject = rayHit.collider.gameObject;
+        } else
+            scanObject = null;
     }
 }
