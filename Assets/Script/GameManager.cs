c2912315defbd7c422760a9b42e1b56f24aa063e
HaeSeungJee <wlgotmd123@naver.com>
2023-02-12 01:30:51 +0900

    B25
     - 대화창 이펙트
     - 초상화 이펙트
     - 타이핑 이펙트

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -7,12 +7,15 @@ public class GameManager : MonoBehaviour
 {
     public TalkManager talkManager;
     public QuestManager questManager;
-    public GameObject talkPanel;
+    public Animator talkPanel;
     public Image portraitImg;
-    public Text talkText;
+    public Animator portraitAmi;
+    public Sprite prevPortrait;
+    public TypeEffector talk;
     public GameObject scanObject;
     public bool isAction;
     public int talkIndex;
+
 
     void Start()
     {
@@ -21,18 +24,28 @@ public class GameManager : MonoBehaviour
 
     public void Action(GameObject scanObj)
     {
+        // Get Current Object
         scanObject = scanObj;
         ObjData objData = scanObj.GetComponent<ObjData>();
         Talk(objData.id, objData.isNPC);
 
-        talkPanel.SetActive(isAction);
+        // Visible Talk for Action
+        talkPanel.SetBool("isShow", isAction);
     }
 
     void Talk(int id, bool isNPC)
     {
         // Set Talk Data
-        int questTalkIndex = questManager.GetQuestTalkIndex(id);
-        string talkData = talkManager.GetTalk(id + questTalkIndex, talkIndex);
+        int questTalkIndex = 0;
+        string talkData = "";
+        if (talk.isAnim) {
+            talk.SetMsg("");
+            return;
+        } else {
+            questTalkIndex = questManager.GetQuestTalkIndex(id);
+            talkData = talkManager.GetTalk(id + questTalkIndex, talkIndex);
+        }
+
 
         // End Talk
         if (talkData == null) {
@@ -44,13 +57,19 @@ public class GameManager : MonoBehaviour
 
         // Continue Talk
         if (isNPC) {
-            talkText.text = talkData.Split(':')[0];
+            talk.SetMsg(talkData.Split(':')[0]);
 
             // Show Portrait
             portraitImg.sprite = talkManager.GetPortrait(id, int.Parse(talkData.Split(':')[1]));
             portraitImg.color = new Color(1, 1, 1, 1);
+            // Animation Portrait
+            if (prevPortrait != portraitImg.sprite) {
+                portraitAmi.SetTrigger("doEffect");
+                prevPortrait = portraitImg.sprite;
+            }
+            
         } else {
-            talkText.text = talkData;
+            talk.SetMsg(talkData);
 
             portraitImg.color = new Color(1, 1, 1, 0);
         }
---
+++ Assets/Script/TypeEffector.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TypeEffector : MonoBehaviour
+{
+    public int CharPerSeconds;
+    public GameObject EndCursor;
+    public bool isAnim;
+    string targetMsg;
+    Text msgText;
+    AudioSource audioSource;
+    int index;
+    float interval;
+
+    private void Awake()
+    {
+        msgText = GetComponent<Text>();    
+        audioSource = GetComponent<AudioSource>();
+    }
+    public void SetMsg(string msg)
+    {
+        if (isAnim) {   // Interrupt
+            msgText.text = targetMsg;
+            CancelInvoke();
+            EffectEnd();
+        } else {
+            targetMsg = msg;
+            EffectStart();
+        }
+    }
+
+    void EffectStart()
+    {
+        msgText.text = "";
+        index = 0;
+        EndCursor.SetActive(false);
+
+        // Start Animation
+        interval = 1.0f / CharPerSeconds;
+        Debug.Log(interval);
+
+        isAnim = true;
+
+        Invoke("Effecting", interval);
+    }
+
+    void Effecting()
+    {
+        if (msgText.text == targetMsg) {
+            EffectEnd();
+            return;
+        }
+        msgText.text += targetMsg[index];
+        index++;
+        // Sound
+        if (targetMsg[index] != ' ' || targetMsg[index] != '.')
+            audioSource.Play();
+        Invoke("Effecting", interval);
+    }
+
+    void EffectEnd()
+    {
+        isAnim = false;
+        EndCursor.SetActive(true);
+    }
+}
