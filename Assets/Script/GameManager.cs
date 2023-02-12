484588efed4c60012e08f3d5a1cae2f816efe009
HaeSeungJee <wlgotmd123@naver.com>
2023-02-13 00:22:30 +0900

    BE26
    저장 / 삭제 버튼 생성
     이슈 : PlayerPrefs를 사용할 때 mac os에서는 어느 위치에 정보가 저장되는지 알 수가 없다.

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -12,14 +12,29 @@ public class GameManager : MonoBehaviour
     public Animator portraitAmi;
     public Sprite prevPortrait;
     public TypeEffector talk;
+    public Text questText;
+    public GameObject menuSet;
     public GameObject scanObject;
+    public GameObject player;
     public bool isAction;
     public int talkIndex;
 
 
     void Start()
     {
-        Debug.Log(questManager.CheckQuest());    
+        GameLoad();
+        questText.text = questManager.CheckQuest();
+    }
+
+    private void Update() 
+    {
+        // Sub menu
+        if (Input.GetButtonDown("Cancel")) {
+            if (menuSet.activeSelf)
+                menuSet.SetActive(false);
+            else
+                menuSet.SetActive(true);
+        }
     }
 
     public void Action(GameObject scanObj)
@@ -51,7 +66,7 @@ public class GameManager : MonoBehaviour
         if (talkData == null) {
             isAction = false;
             talkIndex = 0;
-            Debug.Log(questManager.CheckQuest(id));
+            questText.text = questManager.CheckQuest(id);
             return;
         }
 
@@ -76,5 +91,44 @@ public class GameManager : MonoBehaviour
 
         isAction = true;
         talkIndex++;
+    }
+
+    
+    public void GameSave()
+    {
+        PlayerPrefs.SetFloat("PlayerX", player.transform.position.x);
+        PlayerPrefs.SetFloat("PlayerY", player.transform.position.y);
+        PlayerPrefs.SetInt("QuestId", questManager.questId);
+        PlayerPrefs.SetInt("QuestActionIndex", questManager.questActionIndex);
+        PlayerPrefs.Save();
+
+        menuSet.SetActive(false);
+    }
+
+    public void GameLoad()
+    {
+        if (!PlayerPrefs.HasKey("PlayerX")) {
+            return;
+        }
+        float x = PlayerPrefs.GetFloat("PlayerX");
+        float y = PlayerPrefs.GetFloat("PlayerY");
+        int questId = PlayerPrefs.GetInt("QuestId");
+        int questActionIndex = PlayerPrefs.GetInt("QuestActionIndex");
+
+        player.transform.position = new Vector3(x, y, 0);
+        questManager.questId = questId;
+        questManager.questActionIndex = questActionIndex;
+        questManager.ControlObject();
+    }
+
+    public void GameDelete()
+    {
+        PlayerPrefs.DeleteAll();
+        menuSet.SetActive(false);
+    }
+
+    public void GameExit()
+    {
+        Application.Quit();
     }
 }
--- Assets/Script/TypeEffector.cs
+++ Assets/Script/TypeEffector.cs
@@ -39,7 +39,6 @@ public class TypeEffector : MonoBehaviour
 
         // Start Animation
         interval = 1.0f / CharPerSeconds;
-        Debug.Log(interval);
 
         isAnim = true;
 
